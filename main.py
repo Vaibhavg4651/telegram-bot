b2515a250e5f442484be7da5f931a5869d2491c5
vaibhavg4651 <vaibhav.gupta.2021@msit.in>
2024-09-11 21:59:39 +0530

    fixes

--- main.py
+++ main.py
@@ -101,13 +101,13 @@ application.add_handler(CommandHandler("end", end))
 application.add_handler(MessageHandler(filters.ALL & ~filters.COMMAND, handle_message))
 
 
-def main() -> None:
-    port = 5000
-    global public_url
-    public_url = ngrok.connect(port).public_url
-    logger.info(f"ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{port}\"")
+# def main() -> None:
+#     port = 5000
+#     global public_url
+#     public_url = ngrok.connect(port).public_url
+#     logger.info(f"ngrok tunnel \"{public_url}\" -> \"http://127.0.0.1:{port}\"")
     
-    uvicorn.run(app, port=port)
+#     uvicorn.run(app, port=port)
 
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
